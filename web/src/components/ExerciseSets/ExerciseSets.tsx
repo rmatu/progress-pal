236d02bda3fb52daef82a20be2ecdf8002d5ebe3
Ryszard Matula <rmatula@us.edu.pl>
2021-10-26 22:20:47 +0200

    feature: UI and logic for sending workout

--- web/src/components/ExerciseSets/ExerciseSets.tsx
+++ web/src/components/ExerciseSets/ExerciseSets.tsx
@@ -1,61 +1,136 @@
-import React, { useState } from "react";
-import { IExercise } from "../../constants/exercises";
-import { capitalizeFirstLetter } from "../../utils/stringUtils";
-import { ReactComponent as TrashIconSVG } from "../../assets/svg/trash.svg";
+import React, { useEffect, useState } from "react";
 import { ReactComponent as CancelIcon } from "../../assets/svg/bolderClose.svg";
+import { ReactComponent as TrashIconSVG } from "../../assets/svg/trash.svg";
+import { IExercise } from "../../constants/exercises";
+import { IExportedExercise, ISet } from "../../utils/formSchemas";
+import { capitalizeFirstLetter } from "../../utils/stringUtils";
 import { Button } from "../UI";
 import {
   ExerciseName,
-  PrimaryMuscle,
-  PrimaryMuscles,
   Grid,
   GridItem,
-  Wrapper,
-  SetNumber,
   Input,
+  PrimaryMuscle,
+  PrimaryMuscles,
+  SetNumber,
   TrashIcon,
+  Wrapper,
 } from "./styles";
 
 interface ExerciseSetsProps {
   exercise: IExercise;
   handleDeleteExercise: (exercise: any) => void;
-}
-
-interface ISet {
-  id: number;
-  kg: number | null;
-  reps: number | null;
+  setExerciseWithSets?: React.Dispatch<
+    React.SetStateAction<IExportedExercise[]>
+  >;
+  exerciseWithSets: IExportedExercise[];
 }
 
 const ExerciseSets: React.FC<ExerciseSetsProps> = ({
   exercise,
+  exerciseWithSets,
   handleDeleteExercise,
+  setExerciseWithSets,
 }) => {
-  const [setsAmount, setSetsAmount] = useState<ISet[]>([
-    {
-      id: 1,
-      kg: null,
-      reps: null,
-    },
-  ]);
+  const [exportedExercise, setSetsAmount] = useState<IExportedExercise>({
+    exerciseName: exercise.name,
+    sets: [
+      {
+        id: 1,
+        kg: null,
+        reps: null,
+      },
+    ],
+  });
+
+  const [kgInputErrors, setKgInputErrors] = useState<number[]>([]);
+  const [repsInputErrors, setRepsInputErrors] = useState<number[]>([]);
 
   const handleAddSet = () => {
-    const latestItem = setsAmount[setsAmount.length - 1];
-    setSetsAmount(prev => [
+    const latestItem = exportedExercise.sets[exportedExercise.sets.length - 1];
+    setSetsAmount(prev => ({
       ...prev,
-      {
-        id: latestItem.id + 1,
-        kg: latestItem.kg,
-        reps: latestItem.reps,
-      },
-    ]);
+      sets: [
+        ...prev.sets,
+        {
+          id: latestItem.id + 1,
+          kg: null,
+          reps: null,
+        },
+      ],
+    }));
   };
 
   const handleDelete = (el: ISet) => {
-    if (setsAmount.length === 1) return;
+    if (exportedExercise.sets.length === 1) return;
 
-    setSetsAmount(prev => prev.filter(item => item.id !== el.id));
+    setSetsAmount(prev => ({
+      ...prev,
+      sets: [...prev.sets.filter(item => item.id !== el.id)],
+    }));
   };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, el: ISet) => {
+    const name = e.target.name;
+    const value = Number(e.target.value);
+
+    const minusKey = e.target.value[e.target.value.length - 1];
+
+    if (minusKey === "-") return;
+
+    const arr = { ...exportedExercise, sets: [...exportedExercise.sets] };
+    const idx = arr.sets.findIndex(item => item.id === el.id);
+
+    if (name === "kg" && !el.kg) {
+      setKgInputErrors(prev => prev.filter(id => id !== el.id));
+    }
+
+    if (name === "reps" && !el.reps) {
+      setRepsInputErrors(prev => prev.filter(id => id !== el.id));
+    }
+
+    if (name === "kg") {
+      if (value < 0 || value > 1500) return;
+      arr.sets[idx].kg = value;
+    } else if (name === "reps") {
+      if (value < 0 || value > 100) return;
+      arr.sets[idx].reps = value;
+    }
+
+    setSetsAmount(arr);
+  };
+
+  const handleBlur = (e: React.ChangeEvent<HTMLInputElement>, set: ISet) => {
+    const name = e.target.name;
+    if (name === "kg" && !set.kg) {
+      setKgInputErrors(prev => [...prev, set.id]);
+    } else {
+      setKgInputErrors(prev => prev.filter(id => id !== set.id));
+    }
+
+    if (name === "reps" && !set.reps) {
+      setRepsInputErrors(prev => [...prev, set.id]);
+    } else {
+      setRepsInputErrors(prev => prev.filter(id => id !== set.id));
+    }
+  };
+
+  useEffect(() => {
+    if (setExerciseWithSets) {
+      if (!exerciseWithSets.length) {
+        setExerciseWithSets(prev => [...prev, exportedExercise]);
+      } else {
+        const arr = [...exerciseWithSets];
+        const idx = arr.findIndex(el => el.exerciseName === exercise.name);
+        if (idx < 0) {
+          setExerciseWithSets(prev => [...prev, exportedExercise]);
+        } else {
+          arr[idx] = exportedExercise;
+          setExerciseWithSets(arr);
+        }
+      }
+    }
+  }, [exportedExercise]);
 
   return (
     <Wrapper>
@@ -73,16 +148,31 @@ const ExerciseSets: React.FC<ExerciseSetsProps> = ({
         <GridItem>Kg</GridItem>
         <GridItem>Reps</GridItem>
       </Grid>
-      {setsAmount.map((el, idx) => (
+      {exportedExercise.sets.map((el, idx) => (
         <Grid key={el.id}>
           <GridItem>
             <SetNumber>{idx + 1}</SetNumber>
           </GridItem>
           <GridItem>
-            <Input type="number" />
+            <Input
+              name="kg"
+              type="text"
+              pattern="[0-9]{3}"
+              value={el.kg ? el.kg : ""}
+              onChange={e => handleChange(e, el)}
+              onBlur={e => handleBlur(e, el)}
+              error={!!kgInputErrors.find(id => id === el.id)}
+            />
           </GridItem>
           <GridItem>
-            <Input type="number" />
+            <Input
+              name="reps"
+              type="number"
+              value={el.reps ? el.reps : ""}
+              onChange={e => handleChange(e, el)}
+              onBlur={e => handleBlur(e, el)}
+              error={!!repsInputErrors.find(id => id === el.id)}
+            />
           </GridItem>
           <GridItem>
             <TrashIcon>
