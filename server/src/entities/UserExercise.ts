90422ddf817c21e1b423a0a4294570b0b2a8b3bd
Ryszard Matula <rmatula@us.edu.pl>
2021-11-02 17:53:52 +0100

    update: userExercise entity update

--- server/src/entities/UserExercise.ts
+++ server/src/entities/UserExercise.ts
@@ -9,19 +9,88 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
-
-import {
-  AppMuscle,
-  Category,
-  Equipment,
-  Force,
-  Level,
-  Mechanic,
-} from "./CommonExercise";
-import { ExerciseSet } from "./ExerciseSet";
 import { User } from "./User";
-import { Workout } from "./Workout";
 import { WorkoutExercise } from "./WorkoutExercise";
+
+// ! IF THESE ENUMS ARE IMPORTED FROM ANOTHER FILE IT CAUSES BUGS
+// ! IDK IF THIS CAN BE FIXED, THIS IS TYPEORM BUG
+
+export enum AppMuscle {
+  abdominals = "abdominals",
+  hamstrings = "hamstrings",
+  calves = "calves",
+  shoulders = "shoulders",
+  adductors = "adductors",
+  glutes = "glutes",
+  quadriceps = "quadriceps",
+  biceps = "biceps",
+  forearms = "forearms",
+  abductors = "abductors",
+  triceps = "triceps",
+  chest = "chest",
+  lower_back = "lower back",
+  traps = "traps",
+  middle_back = "middle back",
+  lats = "lats",
+  neck = "neck",
+}
+
+export enum Force {
+  pull = "pull",
+  push = "push",
+  static = "static",
+}
+
+export enum Level {
+  beginner = "beginner",
+  intermediate = "intermediate",
+  expert = "expert",
+}
+
+export enum Mechanic {
+  compound = "compound",
+  isolation = "isolation",
+}
+
+export enum Equipment {
+  body = "body only",
+  machine = "machine",
+  kettlebells = "kettlebells",
+  dumbbell = "dumbbell",
+  cable = "cable",
+  barbell = "barbell",
+  bands = "bands",
+  medicine_ball = "medicine ball",
+  exercise_ball = "exercise ball",
+  e_z_curl_bar = "e-z curl bar",
+  foam_roll = "foam roll",
+  other = "other",
+}
+
+export enum Category {
+  strength = "strength",
+  stretching = "stretching",
+  plyometrics = "plyometrics",
+  strongman = "strongman",
+  powerlifting = "powerlifting",
+  cardio = "cardio",
+  olympic_weightlifting = "olympic weightlifting",
+  crossfit = "crossfit",
+  weighted_bodyweight = "weighted bodyweight",
+  assisted_bodyweight = "assisted bodyweight",
+}
+
+export interface Exercise {
+  name: string;
+  primaryMuscles: AppMuscle[];
+  secondaryMuscles: AppMuscle[];
+  force?: Force;
+  level: Level;
+  mechanic?: Mechanic;
+  equipment?: Equipment;
+  category: Category;
+  instructions: string[];
+}
 
 @ObjectType()
 @Entity()
@@ -35,59 +104,68 @@ export class UserExercise extends BaseEntity {
   @Column({ default: false })
   isCommonExercise: boolean;
 
-  // // Fields
-  // @Field(() => String)
-  // @Column({ type: "text" })
-  // name: string;
-  // @Field(() => [String])
-  // @Column({
-  //   name: "primary_muscles",
-  //   type: "enum",
-  //   enum: AppMuscle,
-  //   array: true,
-  //   default: [],
-  // })
-  // primaryMuscles: AppMuscle[];
-  // @Field(() => [String])
-  // @Column({
-  //   name: "secondary_muscles",
-  //   type: "enum",
-  //   enum: AppMuscle,
-  //   array: true,
-  //   default: [],
-  // })
-  // secondaryMuscles: AppMuscle[];
-  // @Field(() => String, { nullable: true })
-  // @Column({ type: "enum", enum: Force, nullable: true })
-  // force: Force;
-  // @Field(() => String)
-  // @Column({ type: "enum", enum: Level, nullable: true })
-  // level: Level;
-  // @Field(() => String, { nullable: true })
-  // @Column({
-  //   type: "enum",
-  //   enum: Mechanic,
-  //   nullable: true,
-  // })
-  // mechanic: Mechanic;
-  // @Field(() => String, { nullable: true })
-  // @Column({
-  //   type: "enum",
-  //   enum: Equipment,
-  //   nullable: true,
-  // })
-  // equipment: Equipment;
-  // @Field(() => String)
-  // @Column({
-  //   type: "enum",
-  //   enum: Category,
-  //   nullable: true,
-  // })
-  // category: Category;
-  // @Field(() => [String])
-  // @Column({ type: "text", array: true })
-  // instructions: string[];
-  // @Field(() => String)
+  // Fields
+  @Field(() => String)
+  @Column({ type: "text" })
+  name: string;
+
+  @Field(() => [String])
+  @Column({
+    name: "primary_muscles",
+    type: "enum",
+    enum: AppMuscle,
+    array: true,
+    default: [],
+  })
+  primaryMuscles: AppMuscle[];
+
+  @Field(() => [String])
+  @Column({
+    name: "secondary_muscles",
+    type: "enum",
+    enum: AppMuscle,
+    array: true,
+    default: [],
+  })
+  secondaryMuscles: AppMuscle[];
+
+  @Field(() => String, { nullable: true })
+  @Column({ type: "enum", enum: Force, nullable: true })
+  force: Force;
+
+  @Field(() => String)
+  @Column({ type: "enum", enum: Level, nullable: true })
+  level: Level;
+
+  @Field(() => String, { nullable: true })
+  @Column({
+    type: "enum",
+    enum: Mechanic,
+    nullable: true,
+  })
+  mechanic: Mechanic;
+
+  @Field(() => String, { nullable: true })
+  @Column({
+    type: "enum",
+    enum: Equipment,
+    nullable: true,
+  })
+  equipment: Equipment;
+
+  @Field(() => String)
+  @Column({
+    type: "enum",
+    enum: Category,
+    nullable: true,
+  })
+  category: Category;
+
+  @Field(() => [String])
+  @Column({ type: "text", array: true })
+  instructions: string[];
+
+  @Field(() => String)
   @UpdateDateColumn({ type: "timestamp with time zone" })
   updatedAt: Date;
 
