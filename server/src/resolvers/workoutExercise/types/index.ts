0cad80c3966207a2fd2c43c356f44b3f857db454
Ryszard Matula <rmatula@us.edu.pl>
2021-11-30 21:01:26 +0100

    feature: getExerciseChartData

--- server/src/resolvers/workoutExercise/types/index.ts
+++ server/src/resolvers/workoutExercise/types/index.ts
@@ -1,4 +1,4 @@
-import { Field, InputType } from "type-graphql";
+import { Field, InputType, ObjectType } from "type-graphql";
 
 @InputType()
 export class GetExerciseChartDataInput {
@@ -10,4 +10,43 @@ export class GetExerciseChartDataInput {
 
   @Field(() => Date)
   endTime: Date;
+}
+
+@ObjectType()
+export class MaxWeightChartData {
+  @Field(() => String)
+  date: string;
+
+  @Field(() => Number)
+  maxWeight: number;
+}
+
+@ObjectType()
+export class VolumeChartData {
+  @Field(() => String)
+  date: string;
+
+  @Field(() => Number)
+  volume: number;
+}
+
+@ObjectType()
+export class WeightSetChartData {
+  @Field(() => String)
+  date: string;
+
+  @Field(() => [Number])
+  sets: number[];
+}
+
+@ObjectType()
+export class GetExerciseChartDataResponse {
+  @Field(() => [MaxWeightChartData])
+  maxWeightChartData: MaxWeightChartData[];
+
+  @Field(() => [VolumeChartData])
+  volumeChartData: VolumeChartData[];
+
+  @Field(() => [WeightSetChartData])
+  weightSetChartData: WeightSetChartData[];
 }
--- server/src/resolvers/workoutExercise/workoutExerciseResolver.ts
+++ server/src/resolvers/workoutExercise/workoutExerciseResolver.ts
@@ -4,14 +4,17 @@ import { Arg, Ctx, Query, Resolver, UseMiddleware } from "type-graphql";
 import { Between, getRepository } from "typeorm";
 import { WorkoutExercise } from "../../entities/WorkoutExercise";
 import { isAuthenticated } from "../../middleware/isAuthenticated";
-import { GetExerciseChartDataInput } from "./types";
+import {
+  GetExerciseChartDataInput,
+  GetExerciseChartDataResponse,
+} from "./types";
 
 @Resolver(WorkoutExercise)
 export class WorkoutExerciseResolver {
   // ===========================
   // ========= QUERYS ==========
   // ===========================
-  @Query(() => [WorkoutExercise])
+  @Query(() => GetExerciseChartDataResponse)
   @UseMiddleware(isAuthenticated)
   async getExerciseChartData(
     @Arg("input") input: GetExerciseChartDataInput,
@@ -48,7 +51,54 @@ export class WorkoutExerciseResolver {
         ],
       });
 
-      return workoutExercises;
+      const weightSetChartData: { date: string; sets: number[] }[] = [];
+      const maxWeightChartData: { maxWeight: number; date: string }[] = [];
+      const volumeChartData: { volume: number; date: string }[] = [];
+
+      workoutExercises.forEach(exercise => {
+        let volume = 0;
+        let maxWeight = 0;
+        const unOrganizedSets: { set: number; weight: number }[] = [];
+
+        console.log(exercise);
+
+        //@ts-ignore
+        exercise.exerciseSet.forEach(set => {
+          // volumeChartData
+          volume = volume + set.reps * set.weight;
+
+          // maxWeightChartData
+          if (set.weight > maxWeight) {
+            maxWeight = set.weight;
+          }
+
+          unOrganizedSets.push({ set: set.set, weight: set.weight });
+        });
+
+        const organized = unOrganizedSets.sort((a, b) => a.set - b.set);
+        const sets = organized.map(el => el.weight);
+
+        // return weights in kg
+        volume /= 1000;
+        maxWeight /= 1000;
+
+        maxWeightChartData.push({
+          maxWeight,
+          date: moment(exercise.updatedAt).format("DD-MM-YYYY"),
+        });
+
+        volumeChartData.push({
+          volume,
+          date: moment(exercise.updatedAt).format("DD-MM-YYYY"),
+        });
+
+        weightSetChartData.push({
+          sets,
+          date: moment(exercise.updatedAt).format("DD-MM-YYYY"),
+        });
+      });
+
+      return { volumeChartData, maxWeightChartData, weightSetChartData };
     } catch (e) {
       console.log(e);
       return new Error("No exercises");
